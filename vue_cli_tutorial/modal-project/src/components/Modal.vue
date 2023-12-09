ee8687b86821444bbb4867edff8912e57682eac0
sdas1991 <sourangshu_das_tict@yahoo.co.in>
2023-12-08 17:56:22 -0700

    changes for Video 4

---
+++ vue_cli_tutorial/modal-project/src/components/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <p>moal content here</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    
+</script>
+<style>
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop {
+        width: 100%;
+        height: 100%;
+        margin: auto;
+        background: rgba(0, 0, 0, 0.5);
+        position: fixed;
+        top: 0;
+    }
+</style>
